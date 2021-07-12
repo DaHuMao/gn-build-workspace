0ed54836531e52a4370d2066f0e9c80bb5041c65
zhangtongxiao@fenbi.com <zhangtongxiao@fenbi.com>
2021-07-12 19:43:44 +0800

    MOD: 修改编译脚本

--- build_script/env_config.sh
+++ build_script/env_config.sh
@@ -1,9 +1,4 @@
 source ./build_script/tool.sh
-RunAndLog 'git submodule update --init'
-export PATH="./depot_tools:$PATH"
-export DEPOT_TOOLS_UPDATE=0
-cd depot_tools
-RunAndLog 'eval ./gclient 1>/dev/null'
-cd ../
+which gclient || { echo no gclient && exit 1; }
 RunAndLog 'gclient sync --verbose --nohook'
 
